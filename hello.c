97b5f94f2ce5566f573b415eb55000c962e8d3cf
Neata Romeo <romeoneata@gmail.com>
2018-04-20 13:29:25 +0300

    commit 2

--- hello.c
+++ hello.c
@@ -1,5 +1,9 @@
 #include <stdio>
-int main(){
-printf("Hello World!);
+int main(int argc,char *argv[]){
+if(argc!=2){
+printf("USAGE: %s name\n",argv[0]);
+return -1;
+}
+printf("Hello, %s!\n",argv[1]);
 return 0;
 }
